f4397205c8f9afc8fbe3cb00a0f4acf647681671
Max <weber.max.1988@live.de>
2022-04-14 19:59:28 +0200

    🛠 [Feature] To make the OnBoarding screen look more appealing we introduced the lower part of the onBoarding screen.

--- lib/authentication/landing_page.dart
+++ lib/authentication/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:envubi/authentication/onboarding_steps.dart';
+import 'package:envubi/constants.dart';
 import 'package:envubi/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 
@@ -11,24 +12,84 @@ class LandingPage extends StatelessWidget {
     final localizations = AppLocalizations.of(context);
     return Material(
       child: SafeArea(
-        child: PageView(
-          controller: controller,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            OnboardingSlide(
-              StepImages.stepOne,
-              headlineText: localizations.domain_landing_headlineStepOne,
-              bodyText: localizations.domain_landing_bodyStepOne,
+            Expanded(
+              flex: 4,
+              child: PageView(
+                controller: controller,
+                children: [
+                  OnboardingSlide(
+                    StepImages.stepOne,
+                    headlineText: localizations.domain_landing_headlineStepOne,
+                    bodyText: localizations.domain_landing_bodyStepOne,
+                  ),
+                  OnboardingSlide(
+                    StepImages.stepTwo,
+                    headlineText: localizations.domain_landing_headlineStepTwo,
+                    bodyText: localizations.domain_landing_bodyStepTwo,
+                  ),
+                  OnboardingSlide(
+                    StepImages.stepThree,
+                    headlineText:
+                        localizations.domain_landing_headlineStepThree,
+                    bodyText: localizations.domain_landing_bodyStepThree,
+                  ),
+                ],
+              ),
             ),
-            OnboardingSlide(
-              StepImages.stepTwo,
-              headlineText: localizations.domain_landing_headlineStepTwo,
-              bodyText: localizations.domain_landing_bodyStepTwo,
-            ),
-            OnboardingSlide(
-              StepImages.stepThree,
-              headlineText: localizations.domain_landing_headlineStepThree,
-              bodyText: localizations.domain_landing_bodyStepThree,
-            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: Spacings.small * 2),
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(Spacings.small * 4),
+                      topRight: Radius.circular(Spacings.small * 4),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 6,
+                        spreadRadius: -1,
+                        offset: Offset(-1, -1),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: Spacings.small * 2,
+                          vertical: Spacings.small * 2,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text('Sign in'),
+                              ),
+                            ),
+                            const SizedBox(width: Spacings.small),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text('Register'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+abstract class Spacings {
+  static const small = 8.0;
+  static const medium = 16.0;
+  static const large = 24.0;
+  static const xtraLarge = 32.0;
+}
