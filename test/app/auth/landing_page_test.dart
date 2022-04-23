75dae728a30fd20d8a73147ae859e65d7c6d28bd
Max <weber.max.1988@live.de>
2022-04-23 12:01:03 +0200

    [Test] Make widget test to see that all widgets are at the right place of the landing screen

--- test/app/auth/landing_page_test.dart
+++ test/app/auth/landing_page_test.dart
@@ -1,22 +1,34 @@
 import 'package:envubi/auth/landing_page.dart';
+import 'package:envubi/auth/landing_page/onboarding_slide.dart';
 import 'package:envubi/auth/landing_page/progress_dots.dart';
 import 'package:envubi/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+const subject = MaterialApp(
+  localizationsDelegates: [
+    AppLocalizations.delegate,
+  ],
+  home: LandingPage(),
+);
+
 void main() {
+  testWidgets(
+      'WHEN the user opens the first page '
+      'THEN he should see two button and the progress indicator dots',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(subject);
+
+    expect(find.byType(OnboardingSlide), findsOneWidget);
+    expect(find.byType(ProgressIndicatorDot), findsNWidgets(3));
+    expect(find.byType(ElevatedButton), findsNWidgets(2));
+  });
+
   testWidgets(
       'WHEN the user swipes the Onboarding_Slides '
       'THEN the second progress indicator is active',
       (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        localizationsDelegates: [
-          AppLocalizations.delegate,
-        ],
-        home: LandingPage(),
-      ),
-    );
+    await tester.pumpWidget(subject);
 
     await tester.drag(find.byType(PageView), const Offset(-500, 0));
     await tester.pumpAndSettle();
